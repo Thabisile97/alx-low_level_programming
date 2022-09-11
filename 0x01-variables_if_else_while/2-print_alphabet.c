60a7ed78bef7d9325225c0c0d24357b3b71a42bb
root <tshabalalathabisile7@gmail.com>
2022-09-11 11:33:10 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
 #include <stdio.h>
+
 /**
- * main - main is the entry point of the code
+ * main - main is entry point of the program
  *
- * Return: returns a 0 (zero)
+ * Return: returns 0 (zero)
  */
-/* betty style doc for function main goes there */
 int main(void)
-
 {
+	charalphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
 
-	int n, last_n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	last_n = n % 10;
-
-	if (last_n > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
-
-	else if (last_n < 6 && last_n != 0)
-
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
-	else
-		printf("Last digit of %d is %d and is 0\n", n, last_n);
-
+	while (x <= 25)
+	{
+		putchar(alphabet[x]);
+		x++;
+	}
+	putchar('\n');
 	return (0);
 }
