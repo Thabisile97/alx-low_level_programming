11adb70e41d9ab529fefbc34e3d7943b295be0f6
root <tshabalalathabisile7@gmail.com>
2022-09-11 00:29:13 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	char sent[] = "\Programming is like building a multilingual puzzle";
+	char sent[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(sent);
 	return (0);
