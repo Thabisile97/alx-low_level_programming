73b0670b16f9c8d16ed7c2d61754d23de81bfae2
root <tshabalalathabisile7@gmail.com>
2022-09-11 00:15:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,7 @@
 
 int main(void)
 {
-	char sent [] = "with proper grammar, but the outcome is a piece of art,\n";
+	char sent[] = "with proper grammar, but the outcome is a piece of art,\n";
 
 	printf("%s", sent);
 	return (0);
