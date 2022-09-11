4acc0e94c4862050f7ce1472ad81d88352255107
root <tshabalalathabisile7@gmail.com>
2022-09-10 23:41:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -o cisfun $CFILE
+#include <stdio.h>
+
+/**
+ * main - main entry function
+ *
+ *  Return: returns 0(zero)
+ */
+int main(void)
+{
+	char sent[] = "\Programming is like building a multilingual puzzle";
+
+	puts(sent);
+	return (0);
+}
