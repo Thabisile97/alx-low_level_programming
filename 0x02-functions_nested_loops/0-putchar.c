97055b73d7999dd711321bc5624f281680bf2c4d
root <tshabalalathabisile7@gmail.com>
2022-09-13 10:54:11 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *main - main entry of a program
+ *
+ * Rerurn: return 0 (zero)
+ */
+int main(void)
+{
+	/* char alphabet[] = "abcdefghijklkmnopqrstuvwxyz";*/
+	/**
+	 * alphanumeric characters {a-z} {0-9} {special char}
+	 * they start from {a-z} 65-90 and {0-9} 48-57
+	 * {A-Z} 97-12
+	 * ?
+	 */
+	_putchar("_putchar");
+	putchar('\n');
+	return (0);
+}
